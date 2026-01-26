aa57af96b10454daaf46d6c9c32833b6f3051b25
shirokuuun <marcusneorangel23@gmail.com>
2026-01-26 00:46:36 -0500

    message

---
+++ scope_lab.js
@@ -0,0 +1,21 @@
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+{
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
+
+function show() {
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+}
+
+show();
+
+console.log(functionVar);
+console.log(functionLet); 
+console.log(functionConst); 
